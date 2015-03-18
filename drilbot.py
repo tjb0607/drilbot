81cb416dadbe9503ed1430389031cdf973d50d2e
tjb0607 <tjb0607@gmail.com>
2015-03-18 11:46:13 -0700

    verify admins are registered with NickServ

--- drilbot.py
+++ drilbot.py
@@ -12,7 +12,7 @@ tweetnum = 922321981
 interval = 2000
 next_msg = time.time() + 500  # unix timestamp of next time to post
 
-def RandomDrilTweet(irc, st, channel):
+def RandomDrilTweet(irc, channel):
     driltxt = open('dril.txt') 
     num_lines = sum(1 for line in driltxt)
     linenum = random.randint(1, num_lines)
@@ -24,7 +24,7 @@ def RandomDrilTweet(irc, st, channel):
     driltxt.close()
     print("linenum: " + str(linenum))
     matchObj = re.search('(\d+)\|([^\|]+)\|(.*)', line)
-    IrcSend("PRIVMSG " + channel + " :" + matchObj.group(3), irc, st)
+    IrcSend("PRIVMSG " + channel + " :" + matchObj.group(3), irc)
     global tweetnum
     tweetnum = matchObj.group(1)
 
@@ -34,17 +34,28 @@ def DrilTweetLoop(irc, channel):
         global next_msg
         while (next_msg > time.time()):
             time.sleep(1)
-        ts = time.time()
-        st = datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-        RandomDrilTweet(irc, st, channel)
+        RandomDrilTweet(irc, channel)
         next_msg = time.time() + interval
 
-def IrcSend(string, irc, st):
+def IrcSend(string, irc):
+    ts = time.time()
+    st = datetime.fromtimestamp(ts).strftime('%H:%M:%S')
     if re.search("^PRIVMSG NickServ :IDENTIFY ", string):
         print(st + " [ IRC ] --> PRIVMSG NickServ :IDENTIFY *****")
     else:
         print(st + " [ IRC ] --> " + string)
     irc.send(bytes(string + "\r\n", "utf-8"))
+
+def IsRegistered(nick, irc):
+    IrcSend("WHO " + nick, irc)
+    text = irc.recv(2048).decode('utf-8')
+    for line in text.split('\r\n'):
+        print(st + " [ IRC ] <-- " + line)
+        if re.search("352 [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]*r[^ ]* ", line.split(":")[1]):
+            return True
+        else:
+            IrcSend("PRIVMSG " + nick + " :Error: you must be registered with NickServ to use admin commands.", irc)
+            return False
 
 def IrcBot(server, channel, botnick):
     passfile = open('password.txt')
@@ -55,8 +66,8 @@ def IrcBot(server, channel, botnick):
     irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print(st + " [ IRC ] connecting to: " + server)
     irc.connect((server, 6667))
-    IrcSend("USER " + botnick +" "+ botnick +" "+ botnick +" :no", irc, st)
-    IrcSend("NICK " + botnick, irc, st)
+    IrcSend("USER " + botnick +" "+ botnick +" "+ botnick +" :no", irc)
+    IrcSend("NICK " + botnick, irc)
     lastMessageTime = 0
     joined = False
     tweetLoopProcess = threading.Thread(target=DrilTweetLoop, args=(irc, channel))
@@ -77,30 +88,34 @@ def IrcBot(server, channel, botnick):
             global interval
             global next_msg
             if re.search("^PING", line):
-                IrcSend("PONG " + line.split(' ')[1], irc, st)
+                IrcSend("PONG " + line.split(' ')[1], irc)
             elif re.search("001 " + botnick, line):
-                IrcSend("JOIN "+ channel, irc, st)
-                IrcSend("PRIVMSG NickServ :IDENTIFY " + botpass, irc, st)
+                IrcSend("JOIN "+ channel, irc)
+                IrcSend("PRIVMSG NickServ :IDENTIFY " + botpass, irc)
                 tweetLoopProcess.start()
             elif re.search("PRIVMSG " + channel + ' :' + botnick + ': (url|link)', line, re.IGNORECASE):
-                IrcSend("PRIVMSG " + channel + " :" + "https://twitter.com/dril/status/" + str(tweetnum), irc, st)
+                IrcSend("PRIVMSG " + channel + " :" + "https://twitter.com/dril/status/" + str(tweetnum), irc)
             elif re.search("PRIVMSG " + channel + ' :' + botnick + ': post', line, re.IGNORECASE):
-                RandomDrilTweet(irc, st, channel)
+                RandomDrilTweet(irc, channel)
                 next_msg = time.time() + interval
             elif re.search("PRIVMSG " + channel + " :shut up " + botnick, line, re.IGNORECASE):
-                IrcSend("PRIVMSG " + channel + " :" + line.split("!")[0][1:] + ": https://twitter.com/dril/status/922321981", irc, st)
+                IrcSend("PRIVMSG " + channel + " :" + line.split("!")[0][1:] + ": https://twitter.com/dril/status/922321981", irc)
             elif re.search("PRIVMSG " + botnick + ' :(url|link)', line, re.IGNORECASE):
-                IrcSend("PRIVMSG " + line.split('!')[0][1:] + " :https://twitter.com/dril/status/" + str(tweetnum), irc, st)
+                IrcSend("PRIVMSG " + line.split('!')[0][1:] + " :https://twitter.com/dril/status/" + str(tweetnum), irc)
             elif re.search("^:(tjb0607|tinaun|maryxus|rafe)!.* PRIVMSG " + botnick  + " :interval (\d+)$", line, re.IGNORECASE):
-                interval = int(line.split(' ')[-1])
-                IrcSend("PRIVMSG " + line.split('!')[0][1:] + " :interval set to " + str(interval), irc, st)
+                nick = line.split('!')[0][1:]
+                if IsRegistered(nick, irc):
+                    interval = int(line.split(' ')[-1])
+                    IrcSend("PRIVMSG " + nick + " :interval set to " + str(interval), irc)
             elif re.search("PRIVMSG " + botnick + " :interval", line, re.IGNORECASE):
-                IrcSend("PRIVMSG " + line.split('!')[0][1:] + " :interval is " + str(interval), irc, st)
+                IrcSend("PRIVMSG " + line.split('!')[0][1:] + " :interval is " + str(interval), irc)
             elif re.search("^:(tjb0607|tinaun|maryxus|rafe)!.* PRIVMSG " + botnick + " :reset", line, re.IGNORECASE):    # reset timer
-                next_msg = time.time() + interval
-                IrcSend("PRIVMSG " + line.split('!')[0][1:] + " :timer reset", irc, st)
+                nick = line.split('!')[0][1:]
+                if IsRegistered(nick, irc):
+                    next_msg = time.time() + interval
+                    IrcSend("PRIVMSG " + nick + " :timer reset", irc)
             elif re.search("PRIVMSG " + botnick + " :help", line, re.IGNORECASE):
-                IrcSend("PRIVMSG " + line.split('!')[0][1:] + " :`url` returns the url of the last tweet, `post` posts a new random tweet, and `interval` prints the current message interval in seconds. Admin commands are `reset` to reset the timer, and `interval <seconds>` to set the interval. Only tjb0607, tinaun, maryxus, and rafe can use admin commands.", irc, st)
+                IrcSend("PRIVMSG " + line.split('!')[0][1:] + " :`url` returns the url of the last tweet, `post` posts a new random tweet, and `interval` prints the current message interval in seconds. Admin commands are `reset` to reset the timer, and `interval <seconds>` to set the interval. Only tjb0607, can use admin commands.", irc)
     return
 
 #IrcBot("irc.synirc.net", "#bottest", "drilbot2")
